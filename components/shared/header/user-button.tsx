19f3ca996371c948a0c3645912f0e933b7f58292
Takayuki Kitamoto <t.kitamoto@mailfence.com>
2025-05-15 14:03:32 +0900

    Admin Overview

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -60,6 +60,15 @@ const UserButton = async () => {
               Order History
             </Link>
           </DropdownMenuItem>
+
+          {session?.user?.role === 'admin' && (
+            <DropdownMenuItem>
+              <Link href="/admin/overview" className="w-full">
+                Admin
+              </Link>
+            </DropdownMenuItem>
+          )}
+
           <DropdownMenuItem className="p-0 mb-1">
             <form action={signOutUser} className="w-full">
               <Button
