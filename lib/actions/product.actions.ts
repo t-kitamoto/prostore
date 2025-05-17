1d3003437bda66730b6c392a9de1dcf6d9625303
Takayuki Kitamoto <t.kitamoto@mailfence.com>
2025-05-17 15:20:09 +0900

    Drawer

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -171,3 +171,13 @@ export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
     return { success: false, message: formatError(error) };
   }
 }
+
+// Get all categories
+export async function getAllCategories() {
+  const data = await prisma.product.groupBy({
+    by: ['category'],
+    _count: true,
+  });
+
+  return data;
+}
