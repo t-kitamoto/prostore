e17c3afd92a0443c79dd5c05fe9a02c03fa37cd1
Takayuki Kitamoto <t.kitamoto@mailfence.com>
2025-05-17 16:20:29 +0900

    Carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
+import ProductCarousel from '@/components/shared/product/product-carousel';
 import ProductList from '@/components/shared/product/product-list';
-import { getLatestProducts } from '@/lib/actions/product.actions';
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from '@/lib/actions/product.actions';
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </>
   );
