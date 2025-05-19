122db1738278dfa29fc112ab45c635f9492c7152
Takayuki Kitamoto <t.kitamoto@mailfence.com>
2025-05-19 17:25:02 +0900

    Added Webhook

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -204,7 +204,7 @@ export async function approvePayPalOrder(
 }
 
 // Update order to paid
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
